6715fc05f8d382bc70fdfddf70507f6f56059d64
HelgaPataki1983 <88514237+HelgaPataki1983@users.noreply.github.com>
2021-11-05 01:28:35 +0200

    hw18

--- src/test/java/HW18/PriceUaTest.java
+++ src/test/java/HW18/PriceUaTest.java
@@ -1,6 +1,30 @@
 package HW18;
+/*В поисковом поле ввести oculus
+
+        Кликнуть иконку поиска
+
+        Отсортировать результаты от дорогих к дешевым
+
+        Проверить, что товар Oculus Quest 2 256 Gb присутствует в результатах выдачи*/
 
 import UITest.UIBaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class PriceUaTest extends UIBaseTest {
+    @Test
+    public void findProduct(){
+        driver.get("https://price.ua/ua");
+        String searchText =  "oculus";
+        WebElement searchField = driver.findElement(By.xpath("//input[@id='SearchForm_searchPhrase']"));
+        searchField.sendKeys(searchText);
+
+        WebElement searchButton = driver.findElement(By.className("btn-search"));
+        searchButton.click();
+
+        WebElement sortButton = driver.findElement(By.xpath("//a[@class='link ga_cat_sort hight-to-low']"));
+        sortButton.click();
+
+    }
 }
