ba13efae00dd4aaca43163f688cb2589beba402b
HelgaPataki1983 <88514237+HelgaPataki1983@users.noreply.github.com>
2021-11-05 13:16:38 +0200

    Update PriceUaTest.java

--- src/test/java/HW18/PriceUaTest.java
+++ src/test/java/HW18/PriceUaTest.java
@@ -10,13 +10,17 @@ package HW18;
 import UITest.UIBaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class PriceUaTest extends UIBaseTest {
     @Test
-    public void findProduct(){
+    public void findProduct() {
         driver.get("https://price.ua/ua");
-        String searchText =  "oculus";
+        String searchText = "oculus";
+        String itemTitle = "Oculus Quest 2 256 Gb";
         WebElement searchField = driver.findElement(By.xpath("//input[@id='SearchForm_searchPhrase']"));
         searchField.sendKeys(searchText);
 
@@ -25,6 +29,21 @@ public class PriceUaTest extends UIBaseTest {
 
         WebElement sortButton = driver.findElement(By.xpath("//a[@class='link ga_cat_sort hight-to-low']"));
         sortButton.click();
+
+        WebElement foundElementTitle = driver.findElement(By.cssSelector("a[title^='Oculus Quest 2 256 Gb'"));
+        Assert.assertTrue(foundElementTitle.isDisplayed(),itemTitle);
+
+
+
+
+       /* List<WebElement> itemsTitles = driver.findElements(By.className("product-block type2"));
+        for (WebElement itemsTitle : itemsTitles) {
+            if (itemsTitle.getText().equals(itemTitle)) ;
+            // Assert.assertTrue (itemsTitles.contains("Oculus Quest 2 256 Gb"))
+            //System.out.println();
+
+        }*/
+
 
     }
 }
