04cd53af6e1f6bd91c41caa23153b8b1dfd8228c
HelgaPataki1983 <88514237+HelgaPataki1983@users.noreply.github.com>
2021-11-07 00:34:24 +0200

    Update Booking.java

--- src/test/java/HW19/Booking.java
+++ src/test/java/HW19/Booking.java
@@ -1,6 +1,6 @@
-/*
+
 package HW19;
-*/
+
 /*Заходим на сайт https://www.booking.com
         2. Меняем язык на английский
         3.Вводим пункт назначения, к примеру, London
@@ -8,10 +8,11 @@ package HW19;
         5. Нажимаем кнопку Search
         6. После того, как открылся список отелей, проверить, что пункт назначения и даты указаны верно в левой панели Search
         7. В фильтрах в блоке Review Score выбираем Wonderful: 9+
-        8.Делаем проверку, что для всех отелей, отображенных на странице, рейтинг равен 9 и выше*//*
+        8.Делаем проверку, что для всех отелей, отображенных на странице, рейтинг равен 9 и выше*/
 
 import UITest.UIBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -19,9 +20,19 @@ public class Booking extends UIBaseTest {
     @Test
     public void bookHotel(){
         driver.get("https://www.booking.com");
-        WebElement languageButton = driver.findElement(By.className(language-selection));
+        String place = "London";
+        WebElement languageButton = driver.findElement(By.className("bui-avatar__image"));
+        languageButton.click();
 
+        WebElement englishLanguage = driver.findElement(By.xpath("//div[@class='bui-inline-container__main'] [contains(text(),'English (UK)')]"));
+        englishLanguage.click();
+
+        WebElement findPlaceToGo = driver.findElement(By.id("ss"));
+        findPlaceToGo.sendKeys(place, Keys.ENTER);
+
+        WebElement checkIn = driver.findElement(By.className("sb-date-field__display"));
+        checkIn.click();
     }
 
 }
-*/
+
