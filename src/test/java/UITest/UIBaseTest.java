0b87ab059a1be1d24d7bf3e59fb0e228bafec35b
HelgaPataki1983 <88514237+HelgaPataki1983@users.noreply.github.com>
2021-11-28 22:55:10 +0200

    bt

--- src/test/java/UITest/UIBaseTest.java
+++ src/test/java/UITest/UIBaseTest.java
@@ -1,18 +1,23 @@
 package UITest;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import java.time.Duration;
 
 public class UIBaseTest {
+
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected Actions actions;
+    JavascriptExecutor js;
 
-
-    @BeforeTest
+    @BeforeClass(alwaysRun = true)
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
